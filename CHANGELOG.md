0ced246cd4b72cba740b1863ebb8892b5e8ad66a
Hyago Oliveira <hyagogow@gmail.com>
2022-06-28 18:09:27 -0300

    Add JsonUtilitySerializer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [0.1.0] - 2022-01-01
 ### Added
+- Json Utility Serializer
 - Binary Serializer
 - AES Cryptographer
 - GZip Compressor
---
+++ Runtime/Serializers/JsonUtilitySerializer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace ActionCode.Persistence
+{
+    public class JsonUtilitySerializer : ISerializer
+    {
+        public string Extension => "json";
+
+        public string Serialize<T>(T data) => JsonUtility.ToJson(data);
+        public T Deserialize<T>(string value) => JsonUtility.FromJson<T>(value);
+
+        public static bool IsAvailable()
+        {
+#if MODULE_JSON_SERIALIZE
+            return true;
+#else
+            Debug.LogError("Json Module isn't installed. Install it using Package Manager.");
+            return false;
+#endif
+        }
+    }
+}
