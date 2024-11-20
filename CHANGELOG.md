db954329718fe098c5d771bc1ef2fa225f3fe21f
Hyago Oliveira <hyagogow@gmail.com>
2024-11-20 17:22:09 -0300

    Remove IPersistenceSettings interface

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Removed
+- IPersistenceSettings interface
 
 ## [2.2.0] - 2023-03-28
 ### Added
--- Runtime/Settings/PersistenceSettings.cs
+++ Runtime/Settings/PersistenceSettings.cs
@@ -5,8 +5,11 @@ using ActionCode.Cryptography;
 
 namespace ActionCode.Persistence
 {
+    /// <summary>
+    /// Serialize data using encryption and compression.
+    /// </summary>
     [CreateAssetMenu(fileName = "PersistenceSettings", menuName = "ActionCode/Persistence Settings", order = 110)]
-    public sealed class PersistenceSettings : ScriptableObject, IPersistenceSettings
+    public sealed class PersistenceSettings : ScriptableObject
     {
         [Tooltip("The Serializer type to use.")]
         public SerializerType serializer;
@@ -23,17 +26,48 @@ namespace ActionCode.Persistence
         [Tooltip("Whether to save the uncompressed/uncryptographed file for debugging. Only works on Editor.")]
         public bool saveRawFile = true;
 
+        /// <summary>
+        /// Action fired when the save process starts.
+        /// </summary>
         public event Action OnSaveStart;
+
+        /// <summary>
+        /// Action fired when the save process finishes.
+        /// </summary>
         public event Action OnSaveEnd;
 
+        /// <summary>
+        /// Action fired when the load process starts.
+        /// </summary>
         public event Action OnLoadStart;
+
+        /// <summary>
+        /// Action fired when the load process finishes.
+        /// </summary>
         public event Action OnLoadEnd;
 
+        /// <summary>
+        /// Action fired when the save process finishes with an error.
+        /// </summary>
         public event Action<Exception> OnSaveError;
+
+        /// <summary>
+        /// Action fired when the load process finishes with an error.
+        /// </summary>
         public event Action<Exception> OnLoadError;
 
+        /// <summary>
+        /// The current file system.
+        /// </summary>
         public IFileSystem FileSystem { get; private set; }
 
+        /// <summary>
+        /// Saves the given data using the name.
+        /// </summary>
+        /// <typeparam name="T">The generic type of the saved data.</typeparam>
+        /// <param name="data">The data to save.</param>
+        /// <param name="name">The file name.</param>
+        /// <returns>A task operation of the saving process.</returns>
         public async Task<bool> Save<T>(T data, string name)
         {
             CheckFileSystem();
@@ -59,12 +93,25 @@ namespace ActionCode.Persistence
             }
         }
 
-        public async Task<bool> Save<T>(T data, int slot)
+        /// <summary>
+        /// Saves the given data using the slot index.
+        /// </summary>
+        /// <typeparam name="T">The generic type of the saved data.</typeparam>
+        /// <param name="data">The data to save.</param>
+        /// <param name="slotIndex">The slot index to use.</param>
+        /// <returns>A task operation of the saving process.</returns>
+        public async Task<bool> Save<T>(T data, int slotIndex)
         {
-            PlayerPrefs.SetInt(lastSlotKey, slot);
-            return await Save(data, GetSlotName(slot));
+            PlayerPrefs.SetInt(lastSlotKey, slotIndex);
+            return await Save(data, GetSlotName(slotIndex));
         }
 
+        /// <summary>
+        /// Loads using the given name.
+        /// </summary>
+        /// <typeparam name="T">The generic type of the loaded data.</typeparam>
+        /// <param name="name">The file name to load.</param>
+        /// <returns>A task operation of the loading process.</returns>
         public async Task<T> Load<T>(string name)
         {
             CheckFileSystem();
@@ -87,8 +134,19 @@ namespace ActionCode.Persistence
             return default;
         }
 
-        public async Task<T> Load<T>(int slot) => await Load<T>(GetSlotName(slot));
+        /// <summary>
+        /// Loads using the given slot index.
+        /// </summary>
+        /// <typeparam name="T">The generic type of the loaded data.</typeparam>
+        /// <param name="slotIndex">The slot index to use.</param>
+        /// <returns>A task operation of the loading process.</returns>
+        public async Task<T> Load<T>(int slotIndex) => await Load<T>(GetSlotName(slotIndex));
 
+        /// <summary>
+        /// Loads from the last saved slot.
+        /// </summary>
+        /// <typeparam name="T">The generic type of the loaded data.</typeparam>
+        /// <returns>A task operation of the loading process.</returns>
         public async Task<T> LoadLastSlot<T>()
         {
             const int invalidSlot = -1;
@@ -131,15 +189,12 @@ namespace ActionCode.Persistence
 
         private void CheckFileSystem()
         {
-            if (FileSystem == null)
-            {
-                FileSystem = new FileSystem(
-                    serializer,
-                    compressor,
-                    cryptographer,
-                    cryptographerKey
-                );
-            }
+            FileSystem ??= new FileSystem(
+                serializer,
+                compressor,
+                cryptographer,
+                cryptographerKey
+            );
         }
     }
 }
